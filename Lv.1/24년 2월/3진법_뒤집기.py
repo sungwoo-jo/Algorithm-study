d793b22fce16414bf2c6ebf58c8fc0e54e7c18be
sungwoo-jo <sungwoo9671@naver.com>
2024-02-07 22:45:38 +0900

    2월 7일 commit

--- Lv.1/24년 2월/3진법_뒤집기.py
+++ Lv.1/24년 2월/3진법_뒤집기.py
@@ -1,18 +1,46 @@
 # 1. 내가 작성한 코드
 # (내일 divmod 밖에 방법 없는지 보기)
 n = 45
-answer = []
-namuji = 1
 
-mok = n
-while mok > 0:
-    mok //= 3
+def solution(n):
+    answer = ''
+    result = 0
 
-    # print("mok:", mok)
-    namuji = mok % 3
-    # print("namuji:", namuji)
-    answer.append(namuji)
+    mok = n
+    while mok > 0:
+        answer += str(mok % 3)
+        mok //= 3
 
-print(answer)
+    print(answer)
 
-# 2. 최적 코드
+    answer = answer[::-1]
+
+    cnt = 1
+
+    for i in range(len(answer)):
+        print(int(answer[i]) * i * cnt)
+        cnt *= 3
+
+    return result
+
+print(solution(n))
+
+# 2. 참고 코드
+def good_solution(n):
+    answer = 0
+    arr = '' # 삼진법을 임시 저장할 문자열
+
+    while n > 0: # 삼진법 계산
+        arr += str(n % 3)
+        n //= 3
+
+    arr = arr[::-1] # 문자열을 반대로 정렬
+    cnt = 1
+
+    for i in arr: # 다시 삼진법으로 계산
+        answer += int(i) * cnt
+        cnt *= 3
+
+    return answer
+
+print(good_solution(125))
